2267496fc76f3a5b53d2f50829112c0945eac51d
Mustafizur Rahman <mustafiz.tec@gmail.com>
2024-10-18 20:58:44 +0600

    updated

--- lib/data/utlis/urls.dart
+++ lib/data/utlis/urls.dart
@@ -3,6 +3,7 @@ class Urls{
 
   static const String registration = '$_baseUrl/Registration';
   static const String login = '$_baseUrl/Login';
+  static const String addNewTask = '$_baseUrl/createTask';
 
 
 }
